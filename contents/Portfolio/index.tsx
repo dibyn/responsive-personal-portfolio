8585c700b6795c7de85bc141e4ba7c42c4f0309f
dibyn <dibyn@uxcam.com>
2022-10-30 18:55:47 +0545

    add rcci templates for portfolio

---
+++ contents/Portfolio/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './Portfolio.styles';
+
+type PortfolioProps = {};
+
+export const Portfolio: React.FC<PortfolioProps> = props => {
+    return <div />;
+};
