febd3b22a2d866636430f63f2c79ce7dc80745e5
dibyn <dibyn@uxcam.com>
2022-10-30 17:47:15 +0545

    add beer list

---
+++ pages/beer-list.tsx
@@ -0,0 +1,45 @@
+import React, { FormEvent, memo, useCallback, useState, useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { getBeers } from '../api/index.api';
+import RenderBeerList from '../components/pageComponents/RenderBeerList';
+
+const BEER_FETCH_NUMBER = 7
+const index = memo(() => {
+  const [fetchedNumber, setBeerFetchedNumber] = useState(BEER_FETCH_NUMBER);
+  const [scrolled, setScrolled] = useState(false);
+  const { data: beerList = [], isLoading } = useQuery(
+    ['get-beers', fetchedNumber],
+    () => getBeers(fetchedNumber)
+  );
+  const handleLoadMoreImage = useCallback(
+    (e: FormEvent) => {
+      e.preventDefault();
+      const incremented = fetchedNumber + 3;
+      setBeerFetchedNumber(incremented);
+    },
+    [fetchedNumber]
+  );
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        // setBeerFetchedNumber(14)
+        setScrolled(true);
+       }
+      else setScrolled(false);
+    };
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, []);
+  return (
+    <>
+      <RenderBeerList
+        beerList={beerList}
+        handleLoadMoreImage={handleLoadMoreImage}
+        isLoading={isLoading}
+      />
+    </>
+  );
+});
+export default index;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,11 @@
-import React, { FormEvent, memo, useCallback, useState, useEffect } from 'react';
-import { useQuery } from '@tanstack/react-query';
-import { getBeers } from '../api/index.api';
-import RenderBeerList from '../components/pageComponents/RenderBeerList';
+import React from 'react'
 
-const BEER_FETCH_NUMBER = 7
-const index = memo(() => {
-  const [fetchedNumber, setBeerFetchedNumber] = useState(BEER_FETCH_NUMBER);
-  const [scrolled, setScrolled] = useState(false);
-  const { data: beerList = [], isLoading } = useQuery(
-    ['get-beers', fetchedNumber],
-    () => getBeers(fetchedNumber)
-  );
-  const handleLoadMoreImage = useCallback(
-    (e: FormEvent) => {
-      e.preventDefault();
-      const incremented = fetchedNumber + 3;
-      setBeerFetchedNumber(incremented);
-    },
-    [fetchedNumber]
-  );
-  useEffect(() => {
-    const onScroll = () => {
-      if (window.scrollY > 50) {
-        // setBeerFetchedNumber(14)
-        setScrolled(true);
-       }
-      else setScrolled(false);
-    };
-    window.addEventListener('scroll', onScroll);
-    return () => {
-      window.removeEventListener('scroll', onScroll);
-    };
-  }, []);
+type Props = {}
+
+const index = (props: Props) => {
   return (
-    <>
-      <RenderBeerList
-        beerList={beerList}
-        handleLoadMoreImage={handleLoadMoreImage}
-        isLoading={isLoading}
-      />
-    </>
-  );
-});
-export default index;
+    <div>hello world</div>
+  )
+}
+
+export default index
