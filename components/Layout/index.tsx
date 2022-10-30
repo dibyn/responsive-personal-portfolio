e1c51c1ea32db979f09c99d82711367be89fa9df
dibyn <dibyn@uxcam.com>
2022-10-30 17:44:08 +0545

    add meta tags

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -13,18 +13,28 @@ const theme: DefaultTheme = {
   colors: {
     primary: '#49576a',
     secondary: '#0070f3',
+    body: '#fff',
+    text: '#525252'
   },
 };
+
 const Layout = ({ children }) => (
   <QueryClientProvider client={queryClient}>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Head>
         <title>Beer keep</title>
+        <meta charSet="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Get some beer" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap"
           rel="stylesheet"
