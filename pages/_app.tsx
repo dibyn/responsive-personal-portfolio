4083d909a0098b7b32ff0f5de82275a8edc48956
dibyn <dibyn@uxcam.com>
2022-10-30 18:33:36 +0545

    change name to contents

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 // import 'bootstrap/dist/css/bootstrap.css'
-import Layout from '../components/layout';
+import Layout from '../contents/layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
